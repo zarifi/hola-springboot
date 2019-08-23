b4617f59f2bc1940545d930c35d72d7e0b955590
Zarifi <pm322686@corp.westworlds.com>
2019-08-23 16:23:58 -0400

    passed the testing phase

---
+++ src/main/java/com/redhat/examples/holaspringboot/HolaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.redhat.examples.holaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaSpringbootApplication.class, args);
+	}
+
+}
